8efe1b4a5134f55a1eb57bd24e91eb4a5cdf9d11
arunakk9 <arunakk9@gmail.com>
2017-11-15 00:40:14 +0530

    hobutprojects are added 2
    hobut projects are added in advancely..

---
+++ Hubot3/src/com/hobut/Hobutclass1.java
@@ -0,0 +1,12 @@
+package com.hobut;
+
+public class Hobutclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Happy ending");
+System.out.println("Train in siebel");
+
+	}
+
+}
---
+++ Hubot4/src/com/hobut4/Hobutclass2.java
@@ -0,0 +1,11 @@
+package com.hobut4;
+
+public class Hobutclass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" selenium training is coming");
+System.out.println("wallpapers are existing");
+	}
+
+}
